b726d2798dc202759ba15b19bb4cb5f5eceef45a
sittikiat <miikeguru@gmail.com>
2018-03-04 16:26:05 +0700

    update CORS

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ const path = require("path");
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 })
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(session({
     secret: "sittikiat",
     resave: true,
