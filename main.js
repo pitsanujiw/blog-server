563d11da847dbc131e8219ed8e0f760b763bba59
Sittikiat Sujitranon <sittikiat.sujitranon@thomsonreuters.com>
2018-05-09 14:54:36 +0700

    add logger

--- main.js
+++ main.js
@@ -5,21 +5,27 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const path = require("path");
 
+// keep log
+const logger = function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+};
 
-app.use((req, res, next) => {
+const header = function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "localhost:3000");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-})
+};
+
+app.use(logger);
+app.use(header);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
     secret: "sittikiat",
     resave: true,
     saveUninitialized: false
 }));
-
-
 app.use("/api", require("./routes/index.route"));
 app.use("*", (req, res, next) => {
     res.sendFile(path.join(__dirname, "http-status", "404.html"));
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use("/auth", require("./auth/index.route"));
+// app.use("/auth", require("./auth/index.route"));
 
 module.exports = app;
